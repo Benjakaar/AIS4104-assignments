03166e933d86a7c0c44b13235a61438b34f06ca5
benka <benjamin.karlsen@ntnu.no>
2024-08-23 11:52:20 +0200

    Implemented functions to create rotation matrix for rotating about the x-axis.

--- assignment-1/main.cpp
+++ assignment-1/main.cpp
@@ -2,6 +2,27 @@
 
 #include <Eigen/Dense>
 
+double deg_to_rad(double deg)
+{
+    return deg * M_PI / 180;
+}
+
+double rad_to_deg(double rad)
+{
+    return rad * 180 / M_PI;
+}
+
+Eigen::Matrix3d rotate_x(double degrees)
+{
+    double radians = deg_to_rad(degrees);
+    Eigen::Matrix3d matrix;
+    matrix <<
+        1.0, 0.0, 0.0,
+        0.0, std::cos(radians), -std::sin(radians),
+        0.0, std::sin(radians), std::cos(radians);
+    return matrix;
+}
+
 void example(double constant)
 {
     Eigen::Matrix3d identity;
@@ -15,6 +36,8 @@ void example(double constant)
 
 int main()
 {
-    example(2.0);
+    //example(2.0);
+    Eigen::Matrix3d x_0 = rotate_x(45);
+    std::cout << x_0 << std::endl;
     return 0;
 }
